5ece8b0428bf3cc93ffb4d689ae6222f588076ec
Dmitry Pleshkov <dpleshkov.usa@gmail.com>
2024-07-07 16:28:16 -0700

    CSE 115A - Partial UI Skeleton

---
+++ web/js/teams.js
@@ -0,0 +1,75 @@
+const TEAMS = [
+  {
+    name: "FC Bayern München",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000G.svg"
+  },
+  {
+    name: "Borussia Dortmund",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-000007.svg"
+  },
+  {
+    name: "RB Leipzig",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-000017.svg"
+  },
+  {
+    name: "1. FC Union Berlin",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000V.svg"
+  },
+  {
+    name: "SC Freiburg",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000A.svg"
+  },
+  {
+    name: "Bayer 04 Leverkusen",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000B.svg"
+  },
+  {
+    name: "Eintracht Frankfurt",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000F.svg"
+  },
+  {
+    name: "Vfl Wolfsburg",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-000003.svg"
+  },
+  {
+    name: "1. FSV Mainz 05",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-000006.svg"
+  },
+  {
+    name: "Borussia M'gladbach",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-000004.svg"
+  },
+  {
+    name: "Holstein Kiel",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-000N5P.svg"
+  },
+  {
+    name: "TSG Hoffenheim",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-000002.svg"
+  },
+  {
+    name: "SV Werder Bremen",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000E.svg"
+  },
+  {
+    name: "Vfl Bochum 1848",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000S.svg"
+  },
+  {
+    name: "FC Augsburg",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-000010.svg"
+  },
+  {
+    name: "VfB Stuttgart",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000D.svg"
+  },
+  {
+    name: "FC St. Pauli",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000H.svg"
+  },
+  {
+    name: "1. FC Heidenheim 1846",
+    logo: "https://www.bundesliga.com/assets/clublogo/DFL-CLU-000018.svg"
+  }
+]
+
